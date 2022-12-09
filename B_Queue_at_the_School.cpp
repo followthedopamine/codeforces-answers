7a1b1b9d0cf8dd33aee36ff94ee4ea11dc979b4e
followthedopamine <83159854+followthedopamine@users.noreply.github.com>
2022-12-10 08:45:39 +1300

    Solved B_Queue_at_the_School

---
+++ B_Queue_at_the_School.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int arrayLength;
+  int seconds;
+  cin >> arrayLength >> seconds;
+  string line = "";
+  for (int i = 0; i < arrayLength; i++) {
+    string gender;
+    cin >> gender;
+    line += gender;
+  }
+  for (int j = 0; j < seconds; j++) {
+    for (int i = 0; i < line.size() - 1; i++) {
+      if (line[i] == 'B' && line[i + 1] == 'G') {
+        iter_swap(line.begin() + i, line.begin() + i + 1);
+        i++;
+      }
+    }
+  }
+  cout << line;
+}
